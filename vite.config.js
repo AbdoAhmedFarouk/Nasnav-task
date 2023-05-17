82ce8189f63f20caf3b2c62d35c43a5639faaf94
Abdelrahman Ahmed <supersanko2002@gmail.com>
2023-05-17 19:48:07 +0300

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/https://AbdoAhmedFarouk.github.io/Nasnav-task/",
+  base: "/Nasnav-task/",
 });
